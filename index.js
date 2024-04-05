d37d24346f481113bf616b62eb9b0167dc714c7c
zakiabashir <nshafeys0@gmail.com>
2024-04-05 12:41:13 -0700

    push my code

---
+++ index.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+// arrys and variables are both datatypes, but variables aik hi values kly istamal hoty hn or arrys aik list ko store krny kly hoti h
+// such as
+// these are simple variables in which we use data type string, bolean and number
+// let std1 = "ali";
+// let std2 ="ayesha";
+// this is an array data type
+// let student_name = ["ali", "ayesha","mona"]
+// object 
+// {
+//     name: "zakia";
+// type: "string";
+// message: "what are you doing"
+// }
+// this is an object in which we add data
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.45,
+    PKR: 280,
+};
+let answer = await inquirer.prompt([{
+        name: "to",
+        message: " Enter to cruncey",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "from",
+        message: " Enter from cruncey",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: " Enter your amount",
+        type: "number",
+    }
+]);
+let fromamount = currency[answer.from];
+let toamount = currency[answer.from];
+let amount = answer.amount;
+let baseamount = amount / fromamount; //usd base currency
+let convertedamount = baseamount * toamount;
+console.log(convertedamount);
+console.log(fromamount);
+console.log(toamount);
+console.log(amount);
